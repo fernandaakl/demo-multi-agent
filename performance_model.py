2dc19e87e51270311c1a0d4ab8c28dfa564efc7d
Fernanda Lasmar <felasmar@microsoft.com>
2024-11-29 09:02:50 -0300

    feature: adding security checks

--- performance_model.py
+++ performance_model.py
@@ -21,7 +21,7 @@ from azure.ai.evaluation import GroundednessProEvaluator, GroundednessEvaluator
 
 # Initialzing Groundedness and Groundedness Pro evaluators
 groundedness_eval = GroundednessEvaluator(model_config)
-groundedness_pro_eval = GroundednessProEvaluator(azure_ai_project=azure_ai_project, credential=credential)
+#groundedness_pro_eval = GroundednessProEvaluator(azure_ai_project=azure_ai_project, credential=credential)
 
 query_response = dict(
     query="Which tent is the most waterproof?",
@@ -35,7 +35,27 @@ groundedness_score = groundedness_eval(
 )
 print(groundedness_score)
 
-groundedness_pro_score = groundedness_pro_eval(
-    **query_response
-)
-print(groundedness_pro_score)
+# groundedness_pro_score = groundedness_pro_eval(
+#     **query_response
+# )
+# print(groundedness_pro_score)
+
+from azure.ai.evaluation import ViolenceEvaluator
+from azure.identity import DefaultAzureCredential
+credential = DefaultAzureCredential()
+
+# Initializing Violence Evaluator with project information
+violence_eval = ViolenceEvaluator(credential=credential, azure_ai_project=azure_ai_project)
+# Running Violence Evaluator on a query and response pair
+violence_score = violence_eval(query="What is the capital of France?", answer="Paris.")
+print(violence_score)
+
+# Conversation mode
+import json
+
+conversation_str =  """{"messages": [ { "content": "Which tent is the most waterproof?", "role": "user" }, { "content": "The Alpine Explorer Tent is the most waterproof", "role": "assistant", "context": "From the our product list the alpine explorer tent is the most waterproof. The Adventure Dining Table has higher weight." }, { "content": "How much does it cost?", "role": "user" }, { "content": "$120.", "role": "assistant", "context": "The Alpine Explorer Tent is $120."} ] }""" 
+conversation = json.loads(conversation_str)
+
+violence_conv_score = violence_eval(conversation=conversation) 
+
+print(violence_conv_score)
